90a0445f9ba8390401836016950e22e75bd29d27
Levochka108 <d.klochkov@gmail.com>
2023-07-19 20:42:08 +0300

    add new task for GeekBrains

---
+++ homework_4.sql
@@ -0,0 +1,52 @@
+-- Клочков Дмитрий
+USE vk;
+
+/* Задача 1. Подсчитать количество групп, в которые вступил каждый пользователь. */
+select 
+	CONCAT(firstname, ' ', lastname) AS owner,
+	count(*)
+from users u
+join users_communities uc on u.id = uc.user_id
+group by u.id
+order by count(*) desc
+
+
+/* Задача 2. Подсчитать количество пользователей в каждом сообществе.*/
+select
+	count(*),
+	communities.name
+from users_communities 
+join communities on users_communities.community_id = communities.id
+group by communities.id
+
+
+/* Задача 3.
+Пусть задан некоторый пользователь. 
+Из всех пользователей соц. сети найдите человека, который больше всех общался с выбранным пользователем.
+(решение с объединением таблиц) */
+use vk;
+
+select 
+	from_user_id
+	, concat(u.firstname, ' ', u.lastname) as name
+	, count(*) as 'messages count'
+from messages m
+join users u on u.id = m.from_user_id
+where to_user_id = 1
+group by from_user_id
+order by count(*) desc
+limit 1;
+
+-- (решение с вложенными запросами) 
+select 
+	from_user_id 
+	, concat(
+		(select firstname from users where id = m.from_user_id), ' ', 
+		(select lastname from users where id = m.from_user_id)
+	) as name
+	, count(*) cnt
+from messages m
+where to_user_id = 1
+group by from_user_id
+order by cnt desc
+limit 1;
